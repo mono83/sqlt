64c63a99d0ab541be7f8274535eed5b82fe322d5
Gotterdemarung <gotterdemarung@gmail.com>
2024-03-18 18:22:14 +0200

    :sparkles: IterateScan with error from callback propagation

--- iterateScan.go
+++ iterateScan.go
@@ -46,3 +46,51 @@ func IterateScan(rows *sql.Rows, callback func([]string, []*sql.ColumnType, []in
 	}
 	return nil
 }
+
+// IterateScanE scans given rows ony by one passing obtained data to
+// callback function. On any error stops iteration and returns it.
+func IterateScanE(rows *sql.Rows, callback func([]string, []*sql.ColumnType, []interface{}) error) error {
+	if rows == nil {
+		return sql.ErrNoRows
+	}
+	if callback == nil {
+		return nil // No error
+	}
+
+	// Reading column names
+	columnNames, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+	columnTypes, err := rows.ColumnTypes()
+	if err != nil {
+		return err
+	}
+	columnCount := len(columnNames)
+
+	processed := false
+	for rows.Next() {
+		// Building columns and their pointers for
+		// sql.Scan func.
+		columns := make([]interface{}, columnCount)
+		columnPointers := make([]interface{}, columnCount)
+		for i := range columns {
+			columnPointers[i] = &columns[i]
+		}
+
+		// Scanning the result into the column pointers...
+		if err := rows.Scan(columnPointers...); err != nil {
+			return err
+		}
+
+		err = callback(columnNames, columnTypes, columns)
+		if err != nil {
+			return err
+		}
+		processed = true
+	}
+	if !processed {
+		return sql.ErrNoRows
+	}
+	return nil
+}
